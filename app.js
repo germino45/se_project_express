cf55610f9352ba277d4c003796d9ee6aa493d014
germino45 <114361293+germino45@users.noreply.github.com>
2024-02-23 10:57:33 -0600

    refactor: remove js extension in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const indexRouter = require("./routes/index.js");
+const indexRouter = require("./routes/index");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
